858ffb7f06a5bf95e93be7d6068d3b95e1a124a7
deytola <36213634+deytola@users.noreply.github.com>
2018-06-27 10:27:37 +0100

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,27 @@
+// Select color input
+//var paint = $('#colorPicker').val();
+// Select size input
+//var rows = $('#inputHeight').val();
+//var cols = $('#inputWidth').val();
+// When size is submitted by the user, call makeGrid()
+$(document).ready(function() {
+	$('#sizePicker').submit(function makeGrid(grid) {
+    $('table tr').remove();
+// Your code goes here!
+ var rows = $('#inputHeight').val();
+ var cols = $('#inputWidth').val();
+	for (var i = 1; i <= rows; i++){
+		$('table').append('<tr></tr>');
+			for (var j = 1; j <= cols; j++){
+				$('tr:last').append('<td></td>');
+				$('td').attr("class", 'cells');
+			}
+	}
+   grid.preventDefault();
+
+ $('.cells').click(function(event){
+	var paint = $('#colorPicker').val();
+	$(event.target).css('background-color', paint);
+    });
+  });
+});
